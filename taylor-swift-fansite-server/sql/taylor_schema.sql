c14950787106a981ed00d3b8bf008cf6643a6518
aowens6 <austyn.owens1@gmail.com>
2022-05-19 15:45:19 -0500

    initial commit

---
+++ taylor-swift-fansite-server/sql/taylor_schema.sql
@@ -0,0 +1,17 @@
+drop database if exists taylor;
+create database taylor;
+use taylor;
+
+create table song(
+	id int primary key auto_increment,
+    title varchar(50) not null,
+    length int not null,
+    youTubeUrl varchar(200),
+    spotifyUrl varchar(200),
+    releaseYear int not null
+);
+
+create table gossip(
+	id int primary key auto_increment,
+    deets varchar(2048) not null
+);
