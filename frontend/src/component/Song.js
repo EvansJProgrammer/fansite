4347f5e98303d393f6a370fff255c6bde66dccda
aowens6 <austyn.owens1@gmail.com>
2022-05-27 11:58:20 -0500

    added cards to iframes and cleaned up a few errors

--- frontend/src/component/Song.js
+++ frontend/src/component/Song.js
@@ -1,7 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import AuthContext from "../AuthContext";
-import YouTubePlaylist from "./YouTubePlaylist";
 
 function Song() {
 
--- frontend/src/component/YouTubePlaylist.js
+++ frontend/src/component/YouTubePlaylist.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useEffect, useState, useContext } from "react";
+import AuthContext from "../AuthContext";
 
 const YOUTUBE_API_KEY = "AIzaSyCp7SfgXKnVBXEoMvZGarnp_arZ0NK1TMU"
 const YOUTUBE_PLAYLIST_ITEMS_API = "https://www.googleapis.com/youtube/v3/playlistItems";
@@ -7,6 +7,7 @@ const YOUTUBE_PLAYLIST_ITEMS_API = "https://www.googleapis.com/youtube/v3/playli
 const YouTubePlaylist = () => {
 
     const [videoList, setVideoList] = useState([]);
+    const authManager = useContext(AuthContext);
 
     useEffect(() => {
         fetch(`${YOUTUBE_PLAYLIST_ITEMS_API}?part=snippet&playlistId=PLdnEKz0ib5DD8X0JeJ9MgbTLKNESWNlRe&maxResults=10&key=${YOUTUBE_API_KEY}`)
@@ -24,19 +25,22 @@ const YouTubePlaylist = () => {
 
     }, []);
 
-    console.log(videoList);
-
- 
-
     return (
         <>
             {videoList.map((video) => {
-                
-                return (<div>
-                    <iframe width="420" height="315"
-                        src={`https://www.youtube.com/embed/${video.snippet.resourceId.videoId}`}>
-                    </iframe>
-                </div>);
+
+                return (
+                    <div key={video.id} className="container m-5">
+                        <div className="card m-15 w-75">
+                            <div className="card-body float-right">
+                                <h5 className="card-title">{video.snippet.title}</h5>
+                                <iframe title={video.snippet.title} className="" key={video.snippet.resourceId.videoId} width="420" height="315"
+                                    src={`https://www.youtube.com/embed/${video.snippet.resourceId.videoId}`} allowFullScreen>
+                                </iframe>
+                            </div>
+                        </div>
+                    </div>
+                );
             })}
 
         </>
